51e3e01b4b93692a7d7bad4ea2382ed4bb5c6107
Emrul Hoque <108203835+emrul-hoque@users.noreply.github.com>
2022-10-02 19:57:37 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ReflectionExample
+{
+class Program
+{
+    public static void Main (string [] args)
+    {
+        Employee employee = new Employee();
+
+        employee.getInfo();
+
+        var employeeInfo = typeof(Employee);
+
+        var methods = employeeInfo.GetMethods();
+
+        foreach (var item in methods)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+}
